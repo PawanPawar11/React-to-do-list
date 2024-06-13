1ceb5235a1159e2479dc5cd8092c517700dde338
PawanPawar11 <pawanpawar4369@gmail.com>
2024-06-13 11:49:47 +0530

    third commit

--- app/page.jsx
+++ app/page.jsx
@@ -4,15 +4,32 @@ import React, { useState } from 'react'
 function page() {
     const [title, settitle] = useState("")
     const [desc, setdesc] = useState("")
+    const [mainTask, setmainTask] = useState([])
 
     const submitHandler = (e) => {
         e.preventDefault()
-        console.log(title)
-        console.log(desc)
+        setmainTask([...mainTask, {title, desc}])
         settitle("")
         setdesc("")
+        console.log(mainTask)
     }
-  return (
+
+    let renderTask = <h2>No Task Available</h2>
+
+    if(mainTask.length>0) {
+        renderTask = mainTask.map((t, i) => {
+        return ( 
+        <li>
+            <div className='flex justify-start gap-10 mb-5'>
+                <h5 className='text-2xl font-semibold'>{t.title}</h5>
+                <h6 className='text-2xl font-semibold text-white'>{t.desc}</h6>
+            </div>
+        </li>
+        );
+    });
+    }
+
+    return (
     <>
         <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>My Todo List</h1>
         <form onSubmit={submitHandler}>
@@ -34,6 +51,12 @@ function page() {
             }}/>
             <button className='bg-black text-white px-4 py-3 text-2xl font-bold rounded m-5'>Add Task</button>
         </form>
+        <hr />
+        <div className='p-8 bg-slate-400'>
+            <ul>
+                {renderTask}
+            </ul>
+        </div>
     </>
   )
 }
